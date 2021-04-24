6a0bbeba2e2024aed76c6651330fd227fdb8f155
edr <github@rochefort.de>
2021-04-25 00:28:13 +0200

    Properly handle children argument

--- reactjs-helpers.js
+++ reactjs-helpers.js
@@ -1,24 +1,29 @@
 const e = React.createElement;
-function div(config, children) { return e('div', config, children); }
-function h1(config, children) { return e('h1', config, children); }
-function h2(config, children) { return e('h2', config, children); }
-function h3(config, children) { return e('h3', config, children); }
-function h4(config, children) { return e('h4', config, children); }
-function h5(config, children) { return e('h5', config, children); }
-function h6(config, children) { return e('h6', config, children); }
-function p(config, children) { return e('p', config, children); }
-function a(config, children) { return e('a', config, children); }
-function i(config, children) { return e('i', config, children); }
-function img(config, children) { return e('img', config, children); }
-function span(config, children) { return e('span', config, children); }
-function table(config, children) { return e('table', config, children); }
-function thead(config, children) { return e('thead', config, children); }
-function tbody(config, children) { return e('tbody', config, children); }
-function tr(config, children) { return e('tr', config, children); }
-function th(config, children) { return e('th', config, children); }
-function td(config, children) { return e('td', config, children); }
-function form(config, children) { return e('form', config, children); }
-function label(config, children) { return e('label', config, children); }
-function input(config, children) { return e('input', config, children); }
-function select(config, children) { return e('select', config, children); }
-function button(config, children) { return e('button', config, children); }
+function callCreateElement(elementTypeName, args) {
+    var copy = [].slice.call(args);
+    copy.unshift(elementTypeName);
+    return e.apply(null, copy);
+}
+function div(config, children) { return callCreateElement("div", arguments); }
+function h1(config, children) { return callCreateElement("h1", arguments); }
+function h2(config, children) { return callCreateElement("h2", arguments); }
+function h3(config, children) { return callCreateElement("h3", arguments); }
+function h4(config, children) { return callCreateElement("h4", arguments); }
+function h5(config, children) { return callCreateElement("h5", arguments); }
+function h6(config, children) { return callCreateElement("h6", arguments); }
+function p(config, children) { return callCreateElement("p", arguments); }
+function a(config, children) { return callCreateElement("a", arguments); }
+function i(config, children) { return callCreateElement("i", arguments); }
+function img(config, children) { return callCreateElement("img", arguments); }
+function span(config, children) { return callCreateElement("span", arguments); }
+function table(config, children) { return callCreateElement("table", arguments); }
+function thead(config, children) { return callCreateElement("thead", arguments); }
+function tbody(config, children) { return callCreateElement("tbody", arguments); }
+function tr(config, children) { return callCreateElement("tr", arguments); }
+function th(config, children) { return callCreateElement("th", arguments); }
+function td(config, children) { return callCreateElement("td", arguments); }
+function form(config, children) { return callCreateElement("form", arguments); }
+function label(config, children) { return callCreateElement("label", arguments); }
+function input(config, children) { return callCreateElement("input", arguments); }
+function select(config, children) { return callCreateElement("select", arguments); }
+function button(config, children) { return callCreateElement("button", arguments); }
